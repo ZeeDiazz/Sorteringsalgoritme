02c509aa9ec08f61061fc1b9ca3948fbeeebee16
Zee Daizz <Zaidsh02@gmail.com>
2021-09-08 19:41:46 +0200

    Sorteringsalgoritme
    Den bedre version - Lavet af Zaid Sheikh

--- Sorteringsalgoritme.java
+++ Sorteringsalgoritme.java
@@ -1,18 +1,26 @@
 class Sorteringsalgoritme {
 	public static void main(String[] args) {
- 		int [] num = {5,2,47,7,23,13,41,29,53,11};
+ 		int [] num = {5, 2, 47, 7, 23, 13, 41, 29, 53, 11};
+
+		System.out.println("foer Sortering:");
+		printArray(num);
 
 		sort(num);
+
+		System.out.println("");
+
+		System.out.println("Efter Sortering:");
 		printArray(num);
+
  }
 
 	public static int[] sort(int[] arr){
 		for (int i = 1; i <  arr.length; i++) {
 				int key = arr[i];
-				int j = i-1;
+				int j = i - 1;
 
 				while ( j >= 0 && arr[j] > key ) {
-						arr [j+1] = arr[j];
+						arr [j + 1] = arr[j];
 						j--;
 				}
 				arr[j+1] = key;
@@ -21,8 +29,8 @@ class Sorteringsalgoritme {
 	}
 
 	public static void printArray(int[] arr){
-		for(int i = 0; i < arr.length; i++){
-			System.out.println(arr[i]);
+		for(int i: arr){
+			System.out.print(i+" ");
 		}
 
 	}
